73ed90e0ea6711c7b67ea17d2489df568e51b6d2
Nati <natnael.wondmu@gmail.com>
2025-09-10 09:38:00 -0700

    header component testing done

--- tests/example.spec.js
+++
@@ -1,9 +0,0 @@
-// tests/cart.spec.js
-import { test, expect } from '@playwright/test';
-test('should show empty cart message when no items', async ({ page }) => {
-  await page.goto('/cart');
-
-  await expect(
-    page.getByText(/oops! nothing to display/i)
-  ).toBeVisible();
-});
