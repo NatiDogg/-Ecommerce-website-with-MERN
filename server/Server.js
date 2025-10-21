351ae8c106a2aeef9d2859461a6e0a4697f9674b
Nati Dogg <natnael.wondmu@gmail.com>
2025-10-21 05:00:26 -0700

    frontend is ready for production deployment

--- server/Server.js
+++ server/Server.js
@@ -21,7 +21,7 @@ await connectDB() // establish connection to the database
 await connectCloudinary() // set up cloudinary for image storage
 
 // allowed multiple origins
-const allowedOrigins = ["http://localhost:5173"]
+const allowedOrigins = ["http://localhost:5173", "https://ecommerce-website-with-mern.vercel.app/"]
 
 app.post('/stripe',express.raw({type: 'application/json'}),stripeWebhooks)
 
